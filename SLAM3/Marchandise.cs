273ecc6cbabbc77b1d83ed4db37ff55aaf959f6b
Billy Hennin <billy.hennin@gmail.com>
2014-10-15 22:00:44 +0200

    * Refonte graphique
    - Suppression d'élements inutiles
    - Menu mis à jour
    - Preparation du menu
    - Creation de marchandises

---
+++ SLAM3/Marchandise.cs
@@ -0,0 +1,24 @@
+﻿// This program is a private software, based on c# source code.
+// To sell or change credits of this software is forbidden,
+// except if someone approve it from SLAM3 INC. team.
+//  
+// Copyrights (c) 2014 SLAM3 INC. All rights reserved.
+
+namespace SLAM3
+{
+    internal class Marchandise
+    {
+        private int id;
+        private string nom;
+        private double prix;
+        private int quantite;
+
+        public Marchandise(int id, string nom, int quantite, double prix)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.quantite = quantite;
+            this.prix = prix;
+        }
+    }
+}
--- SLAM3/Pages/menu.xaml.cs
+++ SLAM3/Pages/menu.xaml.cs
@@ -5,7 +5,6 @@
 // Copyrights (c) 2014 SLAM3 INC. All rights reserved.
 
 using System;
-using System.Globalization;
 using System.Windows;
 using System.Windows.Media;
 
@@ -17,36 +16,76 @@ namespace SLAM3.Pages
     // ReSharper disable once UnusedMember.Global
     public partial class devis
     {
+        // ReSharper disable once FieldCanBeMadeReadOnly.Local
+        private double prix = 0;
+
         public devis()
         {
             InitializeComponent();
         }
-
-                        
-
+        
         private void BTNAddFeed_click(object sender, RoutedEventArgs e)
         {
-            LabelPrix.Foreground = new SolidColorBrush(Color.FromRgb(0xff, 0x00, 0x00));
-            LabelPrix.Content = !nEstPasUnNombre(TextBoxDevisQte.Text) ? string.Format("{0}€", (2 * Convert.ToInt32(TextBoxDevisQte.Text))) : "Erreur quantité";
+            
         }
 
         private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             BorderBlack.Width = Menu.ActualWidth > 341 ? Menu.ActualWidth - 340 : 1;
-
+            
             BorderBlack.Height = Menu.ActualHeight - 60;
         }
 
         private void TextBoxDevisQte_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
-            TextBoxDevisQte.BorderBrush = nEstPasUnNombre(TextBoxDevisQte.Text) ? 
-                new SolidColorBrush(Color.FromRgb(0xff, 0x00, 0x00)) : new SolidColorBrush();
+            prixArticle(TextBoxDevisQte.Text);
         }
 
         private static bool nEstPasUnNombre(string QTE)
         {
             int value;
             return (QTE.Trim() == "") || !int.TryParse(QTE, out value);
+        }
+
+        private void erreur()
+        {
+            LabelPrix.Content = "Erreur";
+            LabelPrix.Foreground = new SolidColorBrush(Color.FromRgb(0xff, 0x00, 0x00));
+            TextBoxDevisQte.BorderBrush = new SolidColorBrush(Color.FromRgb(0xff, 0x00, 0x00));
+        }
+
+        private void prixArticle(string Text)
+        {
+            if(nEstPasUnNombre(Text))
+            {
+                erreur();
+            } 
+            else
+            {
+                if(Convert.ToInt32(TextBoxDevisQte.Text)==0)
+                {
+                    try
+                    {
+                        erreur();
+                    }
+                    // ReSharper disable once EmptyGeneralCatchClause
+                    catch
+                    {
+                        //This is just like you, you don't get it
+                    }
+                }
+                else
+                {
+                    LabelPrix.Foreground = new SolidColorBrush(Color.FromRgb(0xC1, 0xC1, 0xC1));
+                    LabelPrix.Content = string.Format("{0}€", (prix * Convert.ToInt32(TextBoxDevisQte.Text)));
+                    TextBoxDevisQte.BorderBrush = new SolidColorBrush();
+                }
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
         }
     }
 }
