0ea331eca1250542941f7cb603f89d17c023e57c
Billy Hennin <billy.hennin@gmail.com>
2015-03-05 17:43:57 +0100

    * Soon localisation
    resx or json ?

--- MANAGER/Localisation/Language/LocalisationEn.Designer.cs
+++ MANAGER/Localisation/Language/LocalisationEn.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MANAGER.Localisation {
+namespace MANAGER.Localisation.Language {
     using System;
     
     
@@ -22,14 +22,14 @@ namespace MANAGER.Localisation {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Localisation {
+    public class LocalisationEn {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Localisation() {
+        internal LocalisationEn() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace MANAGER.Localisation {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MANAGER.Localisation.Localisation", typeof(Localisation).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MANAGER.Localisation.Language.LocalisationEn", typeof(LocalisationEn).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
