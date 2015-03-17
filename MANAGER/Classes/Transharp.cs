842ef7d1f548470c09f7199720ed48fc2fc242ef
Billy Hennin <billy.hennin@gmail.com>
2015-03-17 22:09:59 +0100

    * some fix

--- MANAGER/Classes/Transharp.cs
+++ MANAGER/Classes/Transharp.cs
@@ -38,20 +38,20 @@ namespace MANAGER.Classes
 
 		public static String GetTranslation(String key, LangsEnum lang, params Object[] values)
 		{
-			String strToFormat = GetTranslation(key, lang);
-			if (strToFormat != null)
-			{
-				int index = 0;
-				//Replacing <Placeholder> by {0}, {1} etc
-				strToFormat = Regex.Replace(strToFormat, @Placeholder, delegate { return "{" + index++ + "}"; });
-				return String.Format(strToFormat, values); // Format and return the translation
-			}
-			return null; // Translation not found for the given key and lang
+			var strToFormat = GetTranslation(key, lang);
+		    if(strToFormat == null)
+		    {
+		        return null; // Translation not found for the given key and lang
+		    }
+		    var index = 0;
+		    //Replacing <Placeholder> by {0}, {1} etc
+		    strToFormat = Regex.Replace(strToFormat, @Placeholder, delegate { return "{" + index++ + "}"; });
+		    return String.Format(strToFormat, values); // Format and return the translation
 		}
 
 		public static String GetTranslation(String key, LangsEnum lang)
 		{
-			String filePath = GetLangFilePath(lang);
+			var filePath = GetLangFilePath(lang);
 			const Int32 bufferSize = 1024;
 			using (var fileStream = File.OpenRead(filePath))
 			{
@@ -60,8 +60,8 @@ namespace MANAGER.Classes
 					String line;
 					while ((line = streamReader.ReadLine()) != null)
 					{
-						String[] parts = line.Split(new[] {Separator}, 2); // Split only on 1st occurence (we want 2 parts max)
-						String fileKey = parts[0]; // Left part of the separator
+						var parts = line.Split(new[] {Separator}, 2); // Split only on 1st occurence (we want 2 parts max)
+						var fileKey = parts[0]; // Left part of the separator
 						if (!key.Equals(fileKey)) continue; //Not the correct key, we go to next loop
 						return parts[1]; // Right part of the separator
 					}
@@ -72,7 +72,7 @@ namespace MANAGER.Classes
 
 		private static String GetLangFilePath(LangsEnum lang)
 		{
-			StringBuilder str = new StringBuilder();
+			var str = new StringBuilder();
 			str.Append(LangsFolder).Append("/").Append(lang.ToString().ToLower()).Append(LangFileExt);
 			return str.ToString();
 		}
